3ad0136bc1d4bb62650c1a3e30bc4cc4701d0c6e
email <rrizwan1998@gmail.com>
2023-09-25 14:43:44 +0500

    deploy app

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from src.cnnClassifier.utils.common import decodeImage
 from src.cnnClassifier.pipeline.predict import PredictionPipeline
 
 
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
+# os.putenv('LANG', 'en_US.UTF-8')
+# os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
